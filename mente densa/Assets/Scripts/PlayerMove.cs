a0f34de0ed52bd1ab3bf81825b937d2f0aa1922b
HeladodePistacho <th_sola@hotmail.com>
2020-01-14 19:04:21 +0100

    Changed All movement to physics

--- mente densa/Assets/Scripts/PlayerMove.cs
+++ mente densa/Assets/Scripts/PlayerMove.cs
@@ -10,35 +10,47 @@ public class PlayerMove : MonoBehaviour
     public float max_speed = 5.0f;
     float current_speed = 0.0f;
 
+    Rigidbody2D rb;
+
     //Jump Stuff
-    public float gravity = 1.0f;
     public float jump_impulse = 5.0f;
-    public float current_impulse = 0.0f;
-    public float current_gravity = 0.0f;
-    public bool can_jump = true;
+    float current_impulse;
+
+    bool can_jump = true;
+
 
     public bool enable_move = true;
     public bool enable_jump = true;
     public bool enable_action = true;
 
-    float test;
-
-    bool using_pc = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        test = jump_impulse * Time.deltaTime;
+        rb = GetComponent<Rigidbody2D>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enable_move)
+
+
+      
+
+        if (enable_jump)
+        {
+            HandleJump();
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (enable_move)
             HandleAxis();
 
-        if(enable_jump)
-            HandleJump();
     }
 
     void HandleAxis()
@@ -51,7 +63,7 @@ public class PlayerMove : MonoBehaviour
         transform.Translate(current_speed * Time.deltaTime, 0.0f, 0.0f);
 
         //Decelerate
-        if(axis == 0.0f)
+        if (axis == 0.0f)
         {
             if (current_speed > 0.0f)
             {
@@ -66,32 +78,50 @@ public class PlayerMove : MonoBehaviour
             }
             
         }
+
+        
     }
 
     void HandleJump()
     {
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0)) && can_jump)
-        {
-            Debug.Log("Jump");
-            current_gravity = gravity;
-            current_impulse = test;
+      if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0)) && can_jump)
+      {
+          Debug.Log("Jump");
+
             can_jump = false;
-        }
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0.0f, jump_impulse), ForceMode2D.Impulse);
 
-        current_impulse -= current_gravity * Time.deltaTime;
-
-
-        transform.Translate(0.0f, current_impulse, 0.0f);
-
-        if (transform.position.y < 0.0f)
-        {
-            Vector3 position_y_0 = transform.position;
-            position_y_0.y = 0.0f;
-
-            transform.Translate(position_y_0 - transform.position);
-            current_impulse = 0.0f;
-            current_gravity = 0.0f;
-            can_jump = true;
         }
     }
+
+   //private void OnTriggerStay2D(Collider2D collision)
+   //{
+   //    can_jump = true;
+   //    Debug.Log("COLISION tigger");
+   //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        can_jump = true;
+        Debug.Log("COLISION tigger");
+    }
 }
+
+//public class PlayerMove : MonoBehaviour
+//{
+//    public float speed;
+//
+//    Rigidbody2D rb;
+//
+//    private void Start()
+//    {
+//        rb = GetComponent<Rigidbody2D>();
+//    }
+//
+//    private void FixedUpdate()
+//    {
+//        float axis = Input.GetAxisRaw("Horizontal");
+//        rb.MovePosition(transform.position + transform.TransformDirection(axis, 0, 0) * Time.deltaTime * speed);
+//    }
+//}
