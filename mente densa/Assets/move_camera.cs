9485a8631ffe0b510a520a9cbc2bbc9946db5012
HeladodePistacho <th_sola@hotmail.com>
2020-01-20 19:30:10 +0100

    Fall scene almost done

---
+++ mente densa/Assets/move_camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_camera : MonoBehaviour
+{
+    public CameaFollow camera_follow_script;
+    public PlayerMove player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.enable_action = false;
+        player.enable_jump = false;
+        player.enable_move = false;
+        camera_follow_script.y_offsset = 3.0f;
+    }
+}
---
+++ mente densa/Assets/olasoleadoras.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class olasoleadoras : MonoBehaviour
+{
+
+    Animator anim;
+    float tmp = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tmp += Time.deltaTime;
+
+        float value = (Mathf.Sin(tmp) * 0.5f) + 0.5f;
+
+        if (value > 0.9) value = 1.0f;
+        if (value < 0.25) value = 0.0f;
+
+        anim.SetFloat("wavin", value);
+
+        Debug.Log(anim.GetFloat("wavin"));
+    }
+}
