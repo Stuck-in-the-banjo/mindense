09c5c896fdc4f2a4d9e5d37ad8fb81bed34decad
Fran Ruiz <fran152job@gmail.com>
2020-01-20 18:16:45 +0100

    Falling in parallax

---
+++ mente densa/Assets/Scripts/ParallaxVertical.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ParallaxVertical : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startpos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float dist = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
+    }
+}
