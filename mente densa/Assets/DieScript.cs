6eec808324d7dad8677a598fc6d1ea17a5f8190a
Fran Ruiz <fran152job@gmail.com>
2020-01-20 21:54:02 +0100

    Platform scene art and logic ready to rock

---
+++ mente densa/Assets/DieScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DieScript : MonoBehaviour
+{
+    public string sceneToLoad = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("col");
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
